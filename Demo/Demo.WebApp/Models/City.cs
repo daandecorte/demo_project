f704c13e1ae0a84a98852e8780959c4514610aa0
daandc <daan.decorte3@gmail.com>
2025-10-03 09:19:47 +0200

    int->long city population

--- Demo/Demo.WebApp/Models/City.cs
+++ Demo/Demo.WebApp/Models/City.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
-        public int Population { get; set; }
+        public long Population { get; set; }
     }
 }
